5842925b69c13496a473ca6b711abc903d16c4b1
Unknown <heber.f.marques@gmail.com>
2018-04-27 23:51:52 -0300

    Criado contexto do banco de dados

---
+++ Persistence/SitePessoalDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SitePessoal.Persistence
+{
+    public class SitePessoalDbContext : DbContext
+    {
+        public SitePessoalDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ Persistence/UnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SitePessoal.Persistence
+{
+    public class UnitOfWork
+    {
+    }
+}
